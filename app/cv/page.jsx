71bb3260e8d2f031d65dc58fc337e2144ce52f57
Rein Undheim <rein@undheim.io>
2023-09-05 17:14:34 +0200

    feat: add cv section

--- app/cv/page.jsx
+++ app/cv/page.jsx
@@ -1,7 +1,28 @@
+import { CVSection } from "@/components/CVSection";
+
+const sections = [
+  {
+    title: "Erfaring",
+    sectionTitle: "Arbeidssted | Rolle",
+    period: "Måned YYYY - Måned YYYY",
+    description:
+      "En rask beskrivelse av hva arbeidsoppgavene var i erfaringen. Hva tar du med deg fra stedet. Jobbet du med noen andre? Hva var ansvarsområder? Beskriv kort.",
+  },
+  {
+    title: "Utdanning",
+    sectionTitle: "Utdanningssted | Studieretning",
+    period: "Måned YYYY - Måned YYYY",
+    description:
+      "En rask beskrivelse av hva du skulle lære og lærte på studiet. Er det noe spesielt du tar med deg videre? Hva var det du fordypte deg i? Beskriv kort.",
+  },
+];
+
 export default function CV() {
   return (
-    <div className="container mx-auto mt-32">
-      <p>cv</p>
+    <div className="container mx-auto mt-32 gap-16 flex flex-col">
+      {sections.map((section) => {
+        return <CVSection {...section} key={section.title} />;
+      })}
     </div>
   );
 }
---
+++ components/CVSection.jsx
@@ -0,0 +1,16 @@
+export function CVSection({ title, sectionTitle, period, description }) {
+  return (
+    <div className="grid grid-cols-3">
+      <h2 className="col-start-1 col-end-2 text-5xl text-secondary-700">
+        {title}
+      </h2>
+      <div className="flex flex-col col-span-2 gap-2">
+        <div className="flex flex-col">
+          <span className="font-bold">{sectionTitle}</span>
+          <span>{period}</span>
+        </div>
+        <span>{description}</span>
+      </div>
+    </div>
+  );
+}
